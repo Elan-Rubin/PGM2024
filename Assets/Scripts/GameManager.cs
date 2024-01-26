2f3bffec31b8db762bea4ce37a003a94292424c9
elan_rubin <elandanielrubin@gmail.com>
2024-01-26 15:00:20 -0500

    added materials, fixed gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private static GameManager instance;
+    public static GameManager Instance { get { return instance; } }
+    private void Awake()
+    {
+        if (instance != null && instance != this) Destroy(gameObject);
+        else instance = this;
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
