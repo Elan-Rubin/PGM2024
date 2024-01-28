a0875cf97ec86c95acca1fa8c68a7adddca472ac
elan_rubin <elandanielrubin@gmail.com>
2024-01-28 13:20:07 -0500

    sync

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Level : MonoBehaviour
 {
     [SerializeField] private LevelElement element = LevelElement.Wind;
+    public LevelElement Element { get { return element; } }
     void Start()
     {
         
